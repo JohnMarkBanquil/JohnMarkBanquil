5432ec61f23baac4185fd7928651570b7801e424
JohnMarkBanquil <c20245297.banquil@csav.edu.ph>
2026-09-11 09:48:08 +0800

    variables (let&const)

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello world.");
+console.log("Hello, World!");
+const Name = "JohnnMark L. Banquil"
+console.log("Name is: " + Name); 
+
+let Age = 20; 
+console.log("Age is: " + Age); 
+let Number = 09166138837; 
+console.log("Number is: " + Number); 
+let Address = "Victorias City, Philippines"; 
+console.log("Address is: " + Address); 
+
+function greet(name) {
+    return `Good morning, ${name}`;
+}
+    console.log(greet("JohnMark"));
+
+function mdas(firstNumber, secondNumber) {
+    console.log("Multiplication:", firstNumber * secondNumber);
+    console.log("Division:", firstNumber / secondNumber);
+    console.log("Addition:", firstNumber + secondNumber);
+    console.log("Subtraction:", firstNumber - secondNumber);
+}
+
+mdas(5, 3);
+
+    
